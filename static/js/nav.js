05fb1dff5f8ad5e8711b2d213d6d18ab2e1e8aae
bogdantro <sabertoothtri@gmail.com>
2021-12-22 18:51:11 +0200

    sd

--- static/js/nav.js
+++ static/js/nav.js
@@ -18,7 +18,9 @@ function menu(){
   if (window.getComputedStyle(sidebar,null).getPropertyValue("opacity") == '0'){
     hamburger.classList.add('click')  
     sidebar.classList.add('active')
-    sidebarLi.classList.add('active')
+    setTimeout(function(){
+      sidebarLi.classList.add('active')
+    }, 800)
     background.classList.add('active')
     backToTop.classList.add('menu-active')
     logo.classList.add('menu-click')
@@ -35,6 +37,58 @@ function menu(){
     }, 900);
   }  
 };
+// PRICES DROPDOWN
+function prices(){
+  const pricesToggle = document.getElementById('pricesToggle');
+  const prices = document.getElementById('pricesDropdown');
+
+  if (window.getComputedStyle(prices,null).getPropertyValue("display") == 'none'){
+    prices.classList.add('active')
+    pricesToggle.classList.add('click')
+  } else{
+    prices.classList.remove('active')
+    pricesToggle.classList.remove('click')
+  }
+}
+// ABOUT DROPDOWN
+function about(){
+  const aboutToggle = document.getElementById('aboutToggle');
+  const about = document.getElementById('aboutDropdown');
+
+  if (window.getComputedStyle(about,null).getPropertyValue("display") == 'none'){
+    about.classList.add('active')
+    aboutToggle.classList.add('click')
+  } else{
+    about.classList.remove('active')
+    aboutToggle.classList.remove('click')
+  }
+}
+// HELP DROPDOWN
+function help(){
+  const helpToggle = document.getElementById('helpToggle');
+  const help = document.getElementById('helpDropdown');
+
+  if (window.getComputedStyle(help,null).getPropertyValue("display") == 'none'){
+    help.classList.add('active')
+    helpToggle.classList.add('click')
+  } else{
+    help.classList.remove('active')
+    helpToggle.classList.remove('click')
+  }
+}
+// ACCOUNT DROPDOWN
+function account(){
+  const accountToggle = document.getElementById('accountToggle');
+  const account = document.getElementById('accountDropdown');
+
+  if (window.getComputedStyle(account,null).getPropertyValue("display") == 'none'){
+    account.classList.add('active')
+    accountToggle.classList.add('click')
+  } else{
+    account.classList.remove('active')
+    accountToggle.classList.remove('click')
+  }
+}
 // ACCOUNT LOGIN/SIGNUP
 function myaccount(){
   const user = document.getElementById('myAccountIcon');
