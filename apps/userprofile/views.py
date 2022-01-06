b1c933a6d9422cc7f6a5823f9ff3aef3a896520a
bogdantro <sabertoothtri@gmail.com>
2022-01-06 22:40:59 +0200

    dffd

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -5,8 +5,6 @@ from django.contrib.auth.decorators import login_required
 def myaccount(request):
     return render(request, 'core/myaccount.html')
 
-def login(request):
-    return render(request, 'core/login.html')    
 
 def signup(request):
     return render(request, 'core/signup.html')    
