9b2e7b4261424830be773fa3db5df2142bfad755
bogdantro <sabertoothtri@gmail.com>
2023-01-19 19:12:39 +0200

    frhy

---
+++ static/js/product.js
@@ -0,0 +1,4 @@
+window.addEventListener("scroll", function(){
+    const productSearch = document.getElementById('productSearch');
+    productSearch.classList.toggle("sticky", window.scrollY > 55)
+  })
