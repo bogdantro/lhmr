955f232c28e5751343be3333d56979e431d2c773
bogdantro <sabertoothtri@gmail.com>
2023-02-08 17:23:16 +0200

    ad

--- static/js/about.js
+++ static/js/about.js
@@ -2,3 +2,5 @@ window.addEventListener("scroll", function(){
     const aboutQ = document.getElementById('aboutQ');
     aboutQ.classList.toggle("sticky", window.scrollY > 1)
   })
+
+  
--- static/js/category.js
+++ static/js/category.js
@@ -4,3 +4,12 @@ window.addEventListener("scroll", function(){
     const categoryQ = document.getElementById('categoryQ');
     categoryQ.classList.toggle("sticky", window.scrollY > 55)
   })
+
+
+  if ($(window).width() > 1140) { 
+    window.addEventListener("scroll", function(){
+      const categoryQ = document.getElementById('categoryQ');
+      categoryQ.classList.toggle("sticky", window.scrollY > 1)
+    })
+
+  } 
