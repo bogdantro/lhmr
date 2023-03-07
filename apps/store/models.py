919bba61a6d7fafb0a50492e5ab3a16ff8a97ad8
bogdantro <sabertoothtri@gmail.com>
2023-03-07 19:59:01 +0200

    df

--- apps/store/models.py
+++ apps/store/models.py
@@ -27,7 +27,6 @@ class Product(models.Model):
     business_name = models.CharField(max_length=300)
     slug = models.SlugField(max_length=150)
     description = models.TextField(blank=False)
-    short_description = models.TextField(blank=True)
     seo = models.TextField(blank=True)
     price = models.FloatField(max_length=300, default=0, blank=True, null=True)
     address = models.CharField(max_length=500, blank=True)
